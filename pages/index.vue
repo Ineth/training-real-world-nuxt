d0fb6e1955e0f062162b6c53aa32f4d1fa18340e
Thomas De Pauw <thomas.depauw@xerius.be>
2020-09-23 22:31:55 +0200

    axios data call

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard,
+  },
   head() {
     return {
       title: 'Event Listing',
     }
+  },
+  asyncData({ $axios, error }) {
+    console.log('error: ', error)
+    return $axios
+      .get('http://localhost:3001/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'unable to fetch events at this time.',
+        })
+      })
   },
 }
 </script>
