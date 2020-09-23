3b0b81bd54424785ff66c4127b6499ae8d35e7bf
Thomas De Pauw <thomas.depauw@xerius.be>
2020-09-23 21:53:15 +0200

    Add meta-tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,18 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 export default {
+  head() {
+    return {
+      titleTemplate: '%s - Real world events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can find real life events....',
+        },
+      ],
+    }
+  },
   components: {
     NavBar,
   },
